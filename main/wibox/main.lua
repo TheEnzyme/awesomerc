293c9d0a0a3a5483ca412c61549b7590fc282062
LegoSpacy <legospacy@gmail.com>
2014-06-04 18:47:02 +1000

    Cleaned up main wibox code and added awesome icon
    It doesn't feel right without the awesome icon in the top left corner.

--- main/wibox/main.lua
+++ main/wibox/main.lua
@@ -1,12 +1,15 @@
 local awful = require("awful")
 local wibox = require("wibox")
+local beautiful = require("beautiful")
 
 local wibox_main = {}
 
-local wibox_main_textclock = awful.widget.textclock()
+local textclock = awful.widget.textclock()
+local awesomeicon = wibox.widget.imagebox()
+awesomeicon:set_image(beautiful.awesome_icon)
 
-local wibox_main_taglist = {}
-wibox_main_taglist.buttons = awful.util.table.join(
+local taglist = {}
+taglist.buttons = awful.util.table.join(
 
 	-- Left click -> view only that tag.
 	awful.button({ }, 1, awful.tag.viewonly),
@@ -22,8 +25,8 @@ wibox_main_taglist.buttons = awful.util.table.join(
 
 )
 
-local wibox_main_tasklist = {}
-wibox_main_tasklist.buttons = awful.util.table.join(
+local tasklist = {}
+tasklist.buttons = awful.util.table.join(
 	-- Left click
 	awful.button({ }, 1, function(c)
 		if c == client.focus then
@@ -40,49 +43,51 @@ wibox_main_tasklist.buttons = awful.util.table.join(
 	end)
 )
 
-local wibox_main_promptbox = {}
-local wibox_main_layoutbox = {}
+local promptbox = {}
+local layoutbox = {}
 
 -- Adding the widgets.
 for s=1, screen.count() do
 
 	-- Create a promptbox for each screen.
-	wibox_main_promptbox[s] = awful.widget.prompt()
+	promptbox[s] = awful.widget.prompt()
 
 	-- Create a layoutbox for each screen (shows the current layout).
-	wibox_main_layoutbox[s] = awful.widget.layoutbox(s)
+	layoutbox[s] = awful.widget.layoutbox(s)
 
 	-- Taglist
-	wibox_main_taglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, wibox_main_taglist.buttons)
+	taglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist.buttons)
 
 	-- Tasklist
-	wibox_main_tasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, wibox_main_tasklist.buttons)
+	tasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, tasklist.buttons)
 
 
 	-- The actual wibox.
 	wibox_main[s] = awful.wibox{ position = "top", screen = s }
 
+
 	-- Widgets aligned to the left of the wibox.
 	local left_layout = wibox.layout.fixed.horizontal()
-	left_layout:add(wibox_main_taglist[s])
-	left_layout:add(wibox_main_promptbox[s])
+	left_layout:add(awesomeicon)
+	left_layout:add(taglist[s])
+	left_layout:add(promptbox[s])
 
 	-- Widgets aligned to the right.
 	local right_layout = wibox.layout.fixed.horizontal()
 	if s == 1 then
 		right_layout:add(wibox.widget.systray()) -- Add systray only to screen 1.
 	end
-	right_layout:add(wibox_main_textclock)
-	right_layout:add(wibox_main_layoutbox[s])
+	right_layout:add(textclock)
+	right_layout:add(layoutbox[s])
 
 	-- Put it all together.
 	local layout = wibox.layout.align.horizontal()
 	layout:set_left(left_layout)
-	layout:set_middle(wibox_main_tasklist[s])
+	layout:set_middle(tasklist[s])
 	layout:set_right(right_layout)
 
 	wibox_main[s]:set_widget(layout)
 end
 
 -- Set relevant key bindings.
-awful.key( { MODKEY }, "r", function()  wibox_main_promptbox[mouse.screen]:run()  end )
+awful.key( { MODKEY }, "r", function()  promptbox[mouse.screen]:run()  end )
