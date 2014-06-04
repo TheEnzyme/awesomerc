cef03ca250c2490c80bfd031ee43ad64bdb8b3d9
LegoSpacy <legospacy@gmail.com>
2014-06-04 16:30:54 +1000

    Fixed a typo in the runtime error handler

--- main/error_handler.lua
+++ main/error_handler.lua
@@ -1,7 +1,7 @@
 local in_error = false
 awesome.connect_signal("debug::error", function (err)
 	if in_error then return end -- Make sure we don't go into an endless error loop.
-	in_error = tr
+	in_error = true
 
 	naughty.notify{
 		preset = naughty.config.presets.critical,
