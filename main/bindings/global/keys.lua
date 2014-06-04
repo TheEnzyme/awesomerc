3fc235f052a631e6283ea462763f995aa2ffdba1
LegoSpacy <legospacy@gmail.com>
2014-06-04 20:03:04 +1000

    Aligned key definitions to single tabs

--- main/bindings/global/keys.lua
+++ main/bindings/global/keys.lua
@@ -45,57 +45,57 @@ for i = 1, 9 do
 end
 
 globalkeys =  awful.util.table.join(globalkeys,
-		-- Launching applications --
-    awful.key({ MODKEY,           }, "Return", function () awful.util.spawn(APPLICATIONS.terminal) end),
-		awful.key({ MODKEY, "Shift"   }, "Return", function () awful.util.spawn(APPLICATIONS.veditor) end),
-		awful.key({ MODKEY, "Control" }, "Return", function () awful.util.spawn(APPLICATIONS.webbrowser) end),
-		-- ================ --
+	-- Launching applications --
+	awful.key({ MODKEY,           }, "Return", function () awful.util.spawn(APPLICATIONS.terminal) end),
+	awful.key({ MODKEY, "Shift"   }, "Return", function () awful.util.spawn(APPLICATIONS.veditor) end),
+	awful.key({ MODKEY, "Control" }, "Return", function () awful.util.spawn(APPLICATIONS.webbrowser) end),
+	-- ================ --
 
-		awful.key({ MODKEY,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ MODKEY,           }, "Right",  awful.tag.viewnext       ),
-    awful.key({ MODKEY,           }, "Escape", awful.tag.history.restore),
+	awful.key({ MODKEY,           }, "Left",   awful.tag.viewprev       ),
+	awful.key({ MODKEY,           }, "Right",  awful.tag.viewnext       ),
+	awful.key({ MODKEY,           }, "Escape", awful.tag.history.restore),
 
-    awful.key({ MODKEY,           }, "j",
-        function ()
-            awful.client.focus.byidx( 1)
-            if client.focus then client.focus:raise() end
-        end),
-    awful.key({ MODKEY,           }, "k",
-        function ()
-            awful.client.focus.byidx(-1)
-            if client.focus then client.focus:raise() end
-        end),
+	awful.key({ MODKEY,           }, "j",
+		function ()
+			awful.client.focus.byidx( 1)
+			if client.focus then client.focus:raise() end
+		end),
+	awful.key({ MODKEY,           }, "k",
+		function ()
+			awful.client.focus.byidx(-1)
+			if client.focus then client.focus:raise() end
+		end),
 
-    -- Layout manipulation
-    awful.key({ MODKEY, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ MODKEY, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ MODKEY, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ MODKEY, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
-    awful.key({ MODKEY,           }, "u", awful.client.urgent.jumpto),
-    awful.key({ MODKEY,           }, "Tab",
-        function ()
-            awful.client.focus.history.previous()
-            if client.focus then
-                client.focus:raise()
-            end
-        end),
+	-- Layout manipulation
+	awful.key({ MODKEY, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
+	awful.key({ MODKEY, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
+	awful.key({ MODKEY, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
+	awful.key({ MODKEY, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+	awful.key({ MODKEY,           }, "u", awful.client.urgent.jumpto),
+	awful.key({ MODKEY,           }, "Tab",
+			function ()
+				awful.client.focus.history.previous()
+				if client.focus then
+					client.focus:raise()
+				end
+			end),
 
-    awful.key({ MODKEY, "Control" }, "r", awesome.restart),
-    awful.key({ MODKEY, "Shift"   }, "q", awesome.quit),
+	awful.key({ MODKEY, "Control" }, "r", awesome.restart),
+	awful.key({ MODKEY, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ MODKEY,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ MODKEY,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ MODKEY, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ MODKEY, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ MODKEY, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ MODKEY, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ MODKEY,           }, "space", function () awful.layout.inc(LAYOUTS,  1) end),
-    awful.key({ MODKEY, "Shift"   }, "space", function () awful.layout.inc(LAYOUTS, -1) end),
+	awful.key({ MODKEY,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
+	awful.key({ MODKEY,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
+	awful.key({ MODKEY, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
+	awful.key({ MODKEY, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
+	awful.key({ MODKEY, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
+	awful.key({ MODKEY, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+	awful.key({ MODKEY,           }, "space", function () awful.layout.inc(LAYOUTS,  1) end),
+	awful.key({ MODKEY, "Shift"   }, "space", function () awful.layout.inc(LAYOUTS, -1) end),
 
-    awful.key({ MODKEY, "Control" }, "n", awful.client.restore),
+	awful.key({ MODKEY, "Control" }, "n", awful.client.restore),
 
-    -- Menubar
-    awful.key({ MODKEY }, "p", function() menubar.show() end)
+	-- Menubar
+	awful.key({ MODKEY }, "p", function()  menubar.show()  end)
 )
 
 return globalkeys
