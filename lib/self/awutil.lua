804edd067dba8c1f206b3f216b7b2c4c101f0626
LegoSpacy <legospacy@gmail.com>
2014-06-06 15:25:42 +1000

    Added documentation to awutil functions

--- lib/self/awutil.lua
+++ lib/self/awutil.lua
@@ -3,10 +3,14 @@ local awutil = {}
 local awful = require("awful")
 awful.rules = require("awful.rules")
 
+--- Add keys to the global key registry.
+-- @param keys A table of key objects.
 function awutil.add_global_keys(keys)
 	root.keys( awful.util.table.join(root.keys(), table.unpack(keys)) )
 end
 
+--- Add client rules to Awful's rules list.
+-- @param rules A table of rules (which are themselves tables).
 function awutil.add_client_rules(rules)
 	awful.rules.rules = awful.util.table.join(awful.rules.rules, rules)
 end
