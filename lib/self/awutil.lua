59b33f55cf4d032ff659829711310d2b16a7732c
LegoSpacy <legospacy@gmail.com>
2014-06-06 15:17:02 +1000

    Fixed awutil.add_client_rules

--- lib/self/awutil.lua
+++ lib/self/awutil.lua
@@ -1,15 +1,14 @@
 local awutil = {}
 
 local awful = require("awful")
-
-local awesome_rules = require("awful.rules")
+awful.rules = require("awful.rules")
 
 function awutil.add_global_keys(keys)
 	root.keys( awful.util.table.join(root.keys(), table.unpack(keys)) )
 end
 
 function awutil.add_client_rules(rules)
-	awesome_rules = awful.util.table.join(awesome_rules, rules)
+	awful.rules.rules = awful.util.table.join(awful.rules.rules, rules)
 end
 
 return awutil
