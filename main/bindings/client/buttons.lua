2169f342aa5f989c309a7962f48e957722b15c9c
Satvik Sharma <satvik.sharma2@gmail.com>
2015-08-06 14:42:17 +1000

    cleaned up

--- main/bindings/client/buttons.lua
+++
@@ -1,7 +0,0 @@
-local awful = require("awful")
-
-return awful.util.table.join(
-	awful.button({        }, 1, function (c) client.focus = c; c:raise() end),
-	awful.button({ MODKEY }, 1, awful.mouse.client.move),
-	awful.button({ MODKEY }, 3, awful.mouse.client.resize)
-)
